56aab90ae0e1260ce1eb5e1b20b60ee70d846f1a
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-08-13 18:32:40 +0200

    refactor: :zap: added try catch

--- src/utils/DataGenerator.ts
+++ src/utils/DataGenerator.ts
@@ -2,37 +2,42 @@ import { Status, dataModel, status } from "../types";
 
 
 export function generate(hours: number, interval: number) {
-    const data: dataModel[] = []
-    const startTimestamp = new Date();
-    startTimestamp.setHours(startTimestamp.getHours() - hours);
+    try {
+        const data: dataModel[] = []
+        const startTimestamp = new Date();
+        startTimestamp.setHours(startTimestamp.getHours() - hours);
 
-    const statusOptions: status[] = [Status.ON, Status.OFF, Status.INVALID];
-    const keys = ["light", "compressor", "fan", "status"];
+        const statusOptions: status[] = [Status.ON, Status.OFF, Status.INVALID];
+        const keys = ["light", "compressor", "fan", "status"];
 
-    let currentTimestamp = new Date(startTimestamp);
+        let currentTimestamp = new Date(startTimestamp);
 
-    while (currentTimestamp <= new Date()) {
-        const props: { key: number, value: status }[] = []
+        while (currentTimestamp <= new Date()) {
+            const props: { key: number, value: status }[] = []
 
-        keys.forEach((_: string, index: number) => {
-            const randomStatus = statusOptions[Math.floor(Math.random() * statusOptions.length)];
-            props.push({
-                key: index + 1, value: randomStatus
+            keys.forEach((_: string, index: number) => {
+                const randomStatus = statusOptions[Math.floor(Math.random() * statusOptions.length)];
+                props.push({
+                    key: index + 1, value: randomStatus
+                });
             });
-        });
 
-        const element: dataModel = {
-            timestamp: currentTimestamp.toLocaleTimeString(),
-            status: props[3],
-            fan: props[2],
-            compressor: props[1],
-            light: props[0]
-        };
+            const element: dataModel = {
+                timestamp: currentTimestamp.toLocaleTimeString(),
+                status: props[3],
+                fan: props[2],
+                compressor: props[1],
+                light: props[0]
+            };
 
-        data.push(element);
-        currentTimestamp = new Date(currentTimestamp.getTime() + interval * 1000);
+            data.push(element);
+            currentTimestamp = new Date(currentTimestamp.getTime() + interval * 1000);
+        }
+
+        return data;
+    } catch (exception) {
+        console.error("FAIL TO GENERATE DATA", exception)
+        return []
     }
-
-    return data;
 }
 
